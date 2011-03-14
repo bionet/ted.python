09633b7e32f3e6c1daf13c32ba329b119ecafc1d
Lev Givon <lev@columbia.edu>
2011-03-14 11:38:13 -0400

    Add 2d signal generation routine and demo.

---
+++ 2d_demo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Demo of how to construct real 2D trigonometric polynomials.
+"""
+
+import numpy as np
+import trig_poly as tp
+
+# Set matplotlib backend so that plots can be generated without a
+# display:
+import matplotlib
+matplotlib.use('AGG')
+
+import pylab as p
+
+output_name = '2d_demo_'
+output_count = 0
+output_ext = '.png'
+
+if __name__ == '__main__':
+
+    Sx = Sy = 1.0
+    dx = dy = 1/64.0
+    x = np.arange(-Sx/2, Sx/2, dx)
+    y = np.arange(-Sy/2, Sy/2, dy)
+    Mx = My = 4
+
+    S = tp.gen_trig_poly_2d_fft(x, y, Mx, My)
+    S/np.max(np.abs(S))
+    p.imshow(S)
+    p.colorbar()
+    p.draw_if_interactive()
+    p.savefig(output_name + str(output_count) + output_ext)
--- trig_poly.py
+++ trig_poly.py
@@ -6,6 +6,13 @@ Routines for manipulating trigonometric polynomials.
 
 import numpy as np
 
+def crand(*args):
+    """
+    Complex random values in a given shape.
+    """
+    
+    return np.random.rand(*args)+1j*np.random.rand(*args)
+
 def em(m, t, Omega, M):
     """
     Trigonometric polynomial basis function.
@@ -198,4 +205,58 @@ def get_dirichlet_coeffs_fft(u, dt, M):
     
     return am
 
+def gen_trig_poly_2d_fft(x, y, Mx, My):
+    """
+    Construct a 2D trigonometric polynomial.
 
+    Parameters
+    ----------
+    x : numpy.ndarray
+        Points along the X-axis over which to compute the
+        trigonometric polynomial.
+    y : numpy.ndarray
+        Points along the Y-axis over which to compute the
+        trigonometric polynomial.
+
+    Returns
+    -------
+    S : numpy.ndarray
+        Generated signal.
+
+    Notes
+    -----
+    This function uses the FFT to generate the output signal.
+
+    See http://fourier.eng.hmc.edu/e101/lectures/Image_Processing/node6.html
+    for an example of a DFT of a real 2D discrete signal.
+
+    """
+    
+    Nx = len(x)
+    Ny = len(y)
+
+    dx = x[1]-x[0]
+    dy = y[1]-y[0]
+    
+    Sx = max(x)-min(x)
+    Sy = max(y)-min(y)
+    
+    if Mx < 1 or My < 1:
+        raise ValueError('Mx and My must exceed 0')
+    S_fft = np.zeros((Ny, Nx), np.complex)
+    S_fft[0, 0] = np.random.rand()
+    S_fft[0, 1:Mx+1] = crand(Mx)
+    S_fft[0, -Mx:] = np.conj(S_fft[0, 1:Mx+1][::-1])
+
+    S_fft[1:My+1, 0] = crand(My)
+    S_fft[-My:, 0] = np.conj(S_fft[1:My+1, 0][::-1])
+
+    S_fft[1:My+1, 1:Mx+1] = crand(My, Mx)
+    S_fft[-My:, -Mx:] = np.rot90(np.rot90(np.conj(S_fft[1:My+1,
+                                                       1:Mx+1])))
+
+    S_fft[1:My+1, -Mx:] = crand(My, Mx)
+    S_fft[-My:, 1:Mx+1] = np.rot90(np.rot90(np.conj(S_fft[1:My+1,
+                                                          -Mx:])))
+
+    return np.real(np.fft.ifft2(S_fft))
