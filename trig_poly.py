e8a5a1856f385c13f14ee91ae635ef2e4c8825a3
Lev Givon <lev@columbia.edu>
2011-03-14 11:06:48 -0400

    Move filter stuff to separate demo, compute Omega implicitly.

--- trig_poly.py
+++ trig_poly.py
@@ -14,35 +14,37 @@ def em(m, t, Omega, M):
     ----------
     m : int
         Order of basis function.
-    t : ndarray
+    t : numpy.ndarray
         Times over which to compute the function.
     Omega : float
         Bandwidth (in rad/s).
-
+    M : int
+        Trigonometric polynomial order.
+        
     Returns
     -------
-    u : ndarray
-        Basis function `exp(1j*m*Omega*t/M)/sqrt(TM)`, where
-        `TM == 2*pi*M/Omega`.
+    u : numpy.ndarray
+        Basis function `exp(1j*m*Omega*t/M)/sqrt(T)`, where
+        `T == 2*pi*M/Omega`.
     
     """
 
-    TM = 2*np.pi*M/Omega
-    return np.exp(1j*m*Omega*t/M)/np.sqrt(TM)
+    T = 2*np.pi*M/Omega
+    return np.exp(1j*m*Omega*t/M)/np.sqrt(T)
 
-def gen_trig_poly_coeffs(M):
+def gen_dirichlet_coeffs(M):
     """
     Generate random Dirichlet coefficients for a real signal.
 
     Parameters
     ----------
     M : int
-        `2*M+1` complex coefficients are generated.
+        Trigonometric polynomial order.
 
     Returns
     -------
-    am : ndarray
-        An array containing the coefficients.
+    am : numpy.ndarray
+        Array of `2*M+1` Dirichlet coefficients.
         
     """
     
@@ -53,22 +55,20 @@ def gen_trig_poly_coeffs(M):
 
     return am
 
-def gen_trig_poly(t, Omega, am):
+def gen_trig_poly(t, am):
     """
     Construct a trigonometric polynomial with specified Dirichlet coefficients.
 
     Parameters
     ----------
-    t : ndarray
+    t : numpy.ndarray
         Times over which to compute the trigonometric polynomial.
-    Omega : float
-        Bandwidth of signal (in rad/s).
-    am : ndarray
+    am : numpy.ndarray
         Dirichlet coefficients. The length of this array must be odd.
 
     Returns
     -------
-    u : ndarray
+    u : numpy.ndarray
         Generated signal.
         
     """
@@ -80,28 +80,29 @@ def gen_trig_poly(t, Omega, am):
     if not (N % 2):
         raise ValueError('number of coefficients must be odd')
 
+    T = max(t)-min(t)
+    Omega = 2*np.pi*M/T
+    
     u = np.zeros(len(t), np.complex)
     for m in xrange(-M, M+1):
         u += am[m+M]*em(m, t, Omega, M)
 
     return np.real(u)
 
-def gen_trig_poly_fft(t, Omega, am):
+def gen_trig_poly_fft(t, am):
     """
     Construct a trigonometric polynomial with specified Dirichlet coefficients.
 
     Parameters
     ----------
-    t : ndarray
+    t : numpy.ndarray
         Times over which to compute the trigonometric polynomial.
-    Omega : float
-        Bandwidth of signal (in rad/s).
-    am : ndarray
+    am : numpy.ndarray
         Dirichlet coefficients. The length of this array must be odd.
 
     Returns
     -------
-    u : ndarray
+    u : numpy.ndarray
         Generated signal.
 
     Notes
@@ -118,118 +119,83 @@ def gen_trig_poly_fft(t, Omega, am):
         raise ValueError('number of coefficients must be odd')
 
     dt = t[1]-t[0]
-    TM = 2*np.pi*M/Omega
+    T = max(t)-min(t)
+    Omega = 2*np.pi*M/T
     u_fft = np.zeros(len(t), np.complex)
-    u_fft[len(t)-M:] = am[0:M]*np.sqrt(TM)/dt
-    u_fft[0:M+1] = am[M:]*np.sqrt(TM)/dt
+    u_fft[len(t)-M:] = am[0:M]*np.sqrt(T)/dt
+    u_fft[0:M+1] = am[M:]*np.sqrt(T)/dt
 
     return np.real(np.fft.ifft(u_fft))
 
-def get_dir_coeff_inner(u, dt, Omega, M):
+def get_dirichlet_coeffs_inner(u, dt, M):
     """
-    Compute the Dirichlet coefficients of a trigonometric polynomial
-    using inner products.
+    Compute the Dirichlet coefficients of a trigonometric polynomial.
 
+    Parameters
+    ----------
+    u : numpy.ndarray
+        Input signla.
+    dt : float
+        Time resolution (s).
+    M : int
+        Trigonometric polynomial order.
+
+    Returns
+    -------
+    am : numpy.ndarray
+        Array of `2*M+1` Dirichlet coefficients.
+        
     Notes
-    -----
-    Assumes that `u` is defined over times `dt*arange(0, len(t))` and
+    -----    
+    Assumes that `u` is defined over times `dt*arange(0, len(u))` and
     that `dt*len(u)` is equal to the period of the trigonometric polynomial.
 
+    This routine computes the coefficients using inner products.
+    
     """
 
+    T = dt*len(u)
+    Omega = 2*np.pi*M/T
     t = np.arange(0, len(u))*dt
     am = np.empty(2*M+1, np.complex)
-    TM = 2*np.pi*M/Omega
     for m in xrange(-M, M+1):
         am[m+M] = dt*np.sum(u*em(-m, t, Omega, M))
     return am
 
-def get_dir_coeff_fft(u, dt, Omega, M):
+def get_dirichlet_coeffs_fft(u, dt, M):
     """
-    Compute the Dirichlet coefficients of a trigonometric polynomial
-    using the FFT.
+    Compute the Dirichlet coefficients of a trigonometric polynomial.
+
+    Parameters
+    ----------
+    u : numpy.ndarray
+        Input signla.
+    dt : float
+        Time resolution (s).
+    M : int
+        Trigonometric polynomial order.
+
+    Returns
+    -------
+    am : numpy.ndarray
+        Array of `2*M+1` Dirichlet coefficients.
 
     Notes
     -----
-    Assumes that `u` is defined over times `dt*arange(0, len(t))` and
+    Assumes that `u` is defined over times `dt*arange(0, len(u))` and
     that `dt*len(u)` is equal to the period of the trigonometric polynomial.    
 
+    This routine computes the coefficients using the FFT.
+    
     """
 
-    TM = 2*np.pi*M/Omega
+    T = dt*len(u)
+    Omega = 2*np.pi*M/T
     u_fft = np.fft.fft(u)
     am = np.empty(2*M+1, np.complex)
-    am[0:M] = u_fft[-M:]*dt/np.sqrt(TM)
-    am[M:2*M+1] = u_fft[0:M+1]*dt/np.sqrt(TM)
+    am[0:M] = u_fft[-M:]*dt/np.sqrt(T)
+    am[M:2*M+1] = u_fft[0:M+1]*dt/np.sqrt(T)
     
     return am
 
-def filter_trig_poly(u, h, dt, Omega, M):
-    """
-    Filter a trigonometric signal with a filter's impulse response.
-
-    Parameters
-    ----------
-    u : ndarray
-        Input signal.
-    h : ndarray
-        Impulse response of filter.
-    dt : float
-
-    Returns
-    -------
-    v : ndarray
-        Filtered signal.
-        
-    Notes
-    -----
-    Assumes that `u` is defined over times `dt*arange(0, len(t))` and
-    that `dt*len(u)` is equal to the period of the trigonometric polynomial.    
-
-    """
-
-    # Get the Dirichlet coefficients of the signal:
-    am = get_dir_coeff_fft(u, dt, Omega, M)
-
-    # Get the Dirichlet coefficients of the filter:
-    hm = get_dir_coeff_fft(h, dt, Omega, M)
-
-    # Construct the filtered signal using the above coefficients:
-    t = np.arange(0, len(u))*dt
-    v = np.zeros(len(u), np.complex)
-    TM = 2*np.pi*M/Omega
-    for m in xrange(-M, M+1):
-        v += hm[m+M]*am[m+M]*em(m, t, Omega, M)*np.sqrt(TM)/dt
-
-    return np.real(v)
-
-def filter_trig_poly_fft(u, h):
-    """
-    Filter a trigonometric signal with a filter's impulse response.
-
-    Parameters
-    ----------
-    u : ndarray
-        Input signal.
-    h : ndarray
-        Impulse response of filter.
-    dt : float
-
-    Returns
-    -------
-    v : ndarray
-        Filtered signal.
-        
-    Notes
-    -----
-    Assumes that `u` is defined over times `dt*arange(0, len(t))` and
-    that `dt*len(u)` is equal to the period of the trigonometric polynomial.    
-
-    This function uses the FFT to perform the filtering.
-    
-    """
-
-    N = len(u)
-    return np.real(np.fft.ifft(np.fft.fft(np.hstack((u, np.zeros(len(h)-1))))*\
-                               np.fft.fft(np.hstack((h, np.zeros(len(u)-1))))))[0:N]
 
