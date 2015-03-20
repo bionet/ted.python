47c2b27a810fee5d6b13704cb35422e93c21bb0a
Lev Givon <lev@columbia.edu>
2015-03-20 11:14:20 -0400

    Update copyright date in docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'TED'
-copyright = u'2009-2014, Lev Givon'
+copyright = u'2009-2015, Lev Givon'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
